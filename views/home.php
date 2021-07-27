8cd1bbb4782b68d12d858062a1f1283ae2f20338
JoelDeoKL <joeldeocm88@gmail.com>
2021-07-27 14:42:25 +0200

    Retrait de tous les element non important dans le carousel

--- views/home.php
+++ views/home.php
@@ -4,59 +4,34 @@
 	<div id="myCarousel" class="carousel slide">
 		<div class="carousel-inner">
 		  <div class="item active">
-		  <div class="container">
-			<a href="register.html"><img style="width:100%" src="public/themes/images/carousel/1.png" alt="special offers"/></a>
-			<div class="carousel-caption">
-				  <h4>Laptop Deel</h4>
-				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-				</div>
-		  </div>
+		    <div class="container">
+			    <a href="register.html"><img style="width:100%" src="public/themes/images/carousel/1.png" alt="special offers"/></a>
+		    </div>
 		  </div>
 		  <div class="item">
-		  <div class="container">
-			<a href="register.html"><img style="width:100%" src="public/themes/images/carousel/2.png" alt=""/></a>
-				<div class="carousel-caption">
-				  <h4>Second Thumbnail label</h4>
-				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-				</div>
-		  </div>
+              <div class="container">
+                <a href="register.html"><img style="width:100%" src="public/themes/images/carousel/2.png" alt=""/></a>
+              </div>
 		  </div>
 		  <div class="item">
-		  <div class="container">
-			<a href="register.html"><img src="public/themes/images/carousel/3.png" alt=""/></a>
-			<div class="carousel-caption">
-				  <h4>Second Thumbnail label</h4>
-				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-				</div>
-
+              <div class="container">
+                <a href="register.html"><img src="public/themes/images/carousel/3.png" alt=""/></a>
+              <div class="carousel-caption">
 		  </div>
 		  </div>
 		   <div class="item">
-		   <div class="container">
-			<a href="register.html"><img src="public/themes/images/carousel/4.png" alt=""/></a>
-			<div class="carousel-caption">
-				  <h4>Second Thumbnail label</h4>
-				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-				</div>
-
-		  </div>
+               <div class="container">
+                   <a href="register.html"><img src="public/themes/images/carousel/4.png" alt=""/></a>
+               </div>
 		  </div>
 		   <div class="item">
 		   <div class="container">
 			<a href="register.html"><img src="public/themes/images/carousel/5.png" alt=""/></a>
-			<div class="carousel-caption">
-				  <h4>Second Thumbnail label</h4>
-				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-			</div>
 		  </div>
 		  </div>
 		   <div class="item">
 		   <div class="container">
 			<a href="register.html"><img src="public/themes/images/carousel/6.png" alt=""/></a>
-			<div class="carousel-caption">
-				  <h4>Second Thumbnail label</h4>
-				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-				</div>
 		  </div>
 		  </div>
 		</div>
