711112dc2ab01c8dd9f0c4dc1935bdb84cc167ec
JoelDeoKL <joeldeocm88@gmail.com>
2021-07-27 16:32:47 +0200

    Creation de la classe Users

---
+++ entities/Users.php
@@ -0,0 +1,105 @@
+<?php
+
+class Users{
+
+    private $id_user;
+    private $title;
+    private $firstName;
+    private $lastName;
+    private $email;
+    private $password;
+    private $dateNaissance;
+    private $adresse1;
+    private $adresse2;
+    private $numeroTelephone;
+
+
+    public function __construct($id_user, $title, $firstName, $lastName, $email, $password, $dateNaissance, $adresse1, $adresse2, $numeroTelephone){
+        $this->id_user = $id_user;
+        $this->title = $title;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+        $this->password = $password;
+        $this->dateNaissance = $dateNaissance;
+        $this->adresse1 = $adresse1;
+        $this->adresse2 = $adresse2;
+        $this->numeroTelephone = $numeroTelephone;
+    }
+
+    public function getIdUser() {
+        return $this->id_user;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getFirstName() {
+        return $this->firstName;
+    }
+
+    public function getLastName() {
+        return $this->lastName;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getDateNaissance() {
+        return $this->dateNaissance;
+    }
+
+    public function getAdresse1() {
+        return $this->adresse1;
+    }
+
+    public function getAdresse2() {
+        return $this->adresse2;
+    }
+
+    public function getNumeroTelephone() {
+        return $this->numeroTelephone;
+    }
+
+    public function setIdUser($id_user) {
+        $this->id_user = $id_user;
+    }
+
+    public function setFistName($firstName) {
+        $this->firstName = $firstName;
+    }
+
+    public function setLastName($lastName) {
+        $this->lastName = $lastName;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    public function setDateNaissance($dateNaissance) {
+        $this->dateNaissance = $dateNaissance;
+    }
+
+    public function setAdresse1($adresse1) {
+        $this->adresse1 = $adresse1;
+    }
+
+    public function setAdresse2($adresse2) {
+        $this->adresse2 = $adresse2;
+    }
+
+    public function setANumeroTelephone($numeroTelephone) {
+        $this->numeroTelephone = $numeroTelephone;
+    }
+}
